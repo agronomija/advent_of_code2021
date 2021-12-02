fb02bff05782839d15e82345f7630dd45b64613e
Andraz Corel <serviranje@gmail.com>
2021-12-02 19:56:26 +0100

    added second part of first day of code

--- day1.py
+++ day1.py
@@ -1,3 +1,4 @@
+# ADVENT OF CODE DAY1, EXCERCISE 2
 all_bigger = 0
 with open("day1.txt", 'r',  encoding = 'utf-8') as f:
 
@@ -16,3 +17,27 @@ with open("day1.txt", 'r',  encoding = 'utf-8') as f:
 
 
 print(f'number of bigger numbers: {all_bigger}')
+
+
+# ADVENT OF CODE DAY1, EXCERCISE 2
+with open("day1.txt", 'r',  encoding = 'utf-8') as f:
+
+    list_of_measurements = [int(line.strip()) for line in f]
+    print(list_of_measurements)
+    number_of_measurements = len(list_of_measurements)
+    print(number_of_measurements)
+
+    count = 0
+    for i in range(number_of_measurements-3):
+        print(list_of_measurements[i:i+3])
+        print(list_of_measurements[i+1:i+4])
+        first3 = sum(list_of_measurements[i:i+3])
+        second3 = sum(list_of_measurements[i + 1:i + 4])
+        print(first3)
+        print(second3)
+        print(f'{first3} < {second3}')
+
+        if first3 < second3:
+            count += 1
+
+print(count)
